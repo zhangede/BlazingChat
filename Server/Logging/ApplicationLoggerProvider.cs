7b60145d8e7137a0a863523d366c0e9c754a52cd
Feng <zhang7777@gmail.com>
2021-09-09 22:57:27 +0200

    add logging

---
+++ Server/Logging/ApplicationLoggerProvider.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Logging;
+
+namespace BlazingChat.Server.Logging
+{
+    public class ApplicationLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+
+}
